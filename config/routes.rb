1eb8e25498fa2034a875b412a577847a451ccce4
ruba-ruba <gunit1991@ukr.net>
2013-11-19 14:45:46 +0200

    add home#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 FileManager::Application.routes.draw do
+  get "home/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
---
+++ spec/support/controller_macros.rb
@@ -0,0 +1,2 @@
+module ControllerMacros
+end
