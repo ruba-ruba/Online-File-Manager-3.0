56e86b41a011543798b0381011eb9dd345f21862
ruba-ruba <gunit1991@ukr.net>
2013-12-16 17:53:36 +0200

    fixes im mailer and applic_ctrl

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   def current_user_quota    
     if current_user
       quota = current_user.quota
-      size = current_user.items.sum(:file_file_size) if Item.any?
+      size = current_user.items.sum(:file_file_size)
       @taken = size*100.to_f/quota if size
       @taken = @taken.to_s.truncate(4, :omission => "")
       @total = quota
