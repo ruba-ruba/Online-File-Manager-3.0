fda71ddd883403c27ebfc4ce517f09545390809b
ruba-ruba <gunit1991@ukr.net>
2013-12-18 10:26:22 +0200

    get user info using grape api & tokens

--- lib/my_app.rb
+++ lib/my_app.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require 'grape'
 
 module MyApp
@@ -6,8 +7,20 @@ module MyApp
     format :json
     default_format :json
 
-    get "/v1" do
-      "text"
+    get "/v1/:token" do
+      user = User.find_by_token params[:token]
+      if user
+        info = {}
+        total_quota = user.quota
+        left_quota = user.items.sum(:file_file_size)
+        info[:email] = user.email
+        info[:total_quota] = total_quota
+        info[:used] = left_quota
+        info[:left_quota] = total_quota - left_quota
+        info
+      else
+        'you have wrong api key'
+      end
     end
 
 
