d3a8fffad41dc53a8f33fe6c9de1c0a34c927fa7
vladislav-gorbenko <gorbenko.vladislav@gmail.com>
2013-12-18 08:25:54 -0800

    create test: destroy, show, edit, create and update  for Items

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -19,5 +19,75 @@ describe ItemsController do
     end
   end
 
+  describe "DELETE destroy" do
+    let!(:item) {FactoryGirl.create(:item)}
+    it "delete item" do
+      request.env["HTTP_REFERER"]="/" 
+      expect{delete :destroy, id: item.id}.to change(Item,:count).by(-1) 
+    end
+  end
+
+  describe 'POST create' do
+    let(:folder) { FactoryGirl.create(:folder) }
+    it 'create new item without folder' do
+      file =  fixture_file_upload('/test.csv', 'text/csv')
+      item = {folder_id: nil, :file => file } 
+      expect{ post :create, item: item }.to change(Item,:count).by(1)
+    end
+
+    it 'create new item with folder' do
+      file =  fixture_file_upload('/test.csv', 'text/csv')
+      item = {folder_id: folder.id, :file => file } 
+      expect{ post :create, item: item }.to change(Item,:count).by(1)
+      folder.items.should have(1).item
+    end
+
+    it 'create new item with folder' do 
+      expect{ post :create, item: FactoryGirl.attributes_for(:item, file_file_name: nil) }.to change(Item,:count).by(0)
+    end
+  end
+
+  describe "PUT update" do
+    let!(:old_item) {FactoryGirl.create(:item, :file_file_name =>"123123")}
+    let!(:folder) {FactoryGirl.create(:folder, parent_id: Folder.first.id)}
+    describe "valid attributes" do
+      it "update item" do
+        file =  fixture_file_upload('/test.csv', 'text/csv')
+        item = {folder_id: folder.id, :file => file } 
+        put :update, {id: old_item.id, item: item}
+        old_item.reload
+        old_item.file_file_name.should eq("test.csv")
+      end
+    end
+    describe "invalid attributes" do
+      it "should not update item" do
+        item = {folder_id: folder.id, :file => nil} 
+        put :update, {id: old_item.id, item: item}
+        old_item.reload
+        old_item.file_file_name.should eq("123123")
+      end
+    end
+  end
+
+  describe 'GET edit' do
+    let(:item){FactoryGirl.create(:item)} 
+    it 'should render edit' do
+      get :edit, id: item.id
+      response.should be_success
+      response.should render_template :edit
+    end
+  end
+
+  describe 'GET show' do
+    let(:item){FactoryGirl.create(:item)} 
+    it 'should render show' do
+      get :show, id: item.id
+      response.should be_success
+      response.should render_template :show
+    end
+  end
+
+
+
 
 end
