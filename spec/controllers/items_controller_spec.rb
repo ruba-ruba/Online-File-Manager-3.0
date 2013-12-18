7b5e8770a54fe8cabcd7dac2b4b2065706704e0b
vladislav-gorbenko <gorbenko.vladislav@gmail.com>
2013-12-18 05:41:47 -0800

    test index for Items

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -2,4 +2,22 @@ require 'spec_helper'
 
 describe ItemsController do
 
+  login_user
+
+  describe 'GET new' do
+    it 'should render new' do
+      get :new
+      response.should be_success
+      response.should render_template :new
+    end
+
+    it 'should render new with folder id' do
+      folder = FactoryGirl.create(:folder)
+      get :new, :folder_id => folder.id
+      response.should be_success
+      response.should render_template :new
+    end
+  end
+
+
 end
