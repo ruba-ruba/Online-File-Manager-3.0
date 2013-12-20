c906144ac2b4007281e6d7524de33fef93d980c4
ruba-ruba <gunit1991@ukr.net>
2013-12-20 14:38:58 +0200

    change addTokens migration with skip validations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131219153357) do
+ActiveRecord::Schema.define(:version => 20131220122800) do
 
   create_table "folders", :force => true do |t|
     t.string   "title"
