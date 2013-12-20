190ea67d077279da28f802e053360003bb429072
ruba-ruba <gunit1991@ukr.net>
2013-12-20 15:08:36 +0200

    improve readme, add rescue for grape

--- lib/users_api.rb
+++ lib/users_api.rb
@@ -3,10 +3,15 @@ module UsersApi
     prefix "api"
     format :json
     default_format :json
+
     helpers do
       def current_user
         @current_user ||= User.find_by_token params[:token]
       end
+    end
+
+    rescue_from NoMethodError do |e|
+      error_response({ message: "token is invalid" })
     end
 
     get "/v1/:token" do
