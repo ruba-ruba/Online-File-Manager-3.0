afc2535c120735e2b75f1f45fbccd2ee3abd0c5a
ruba-ruba <gunit1991@ukr.net>
2013-12-27 12:16:11 +0200

    remove pry

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,7 +1,6 @@
 class VotesController < ApplicationController
   def vote
     @vote = Vote.new(user_id: current_user.id, comment_id: params[:comment])
-        binding.pry
     respond_to do |format|
       if @vote.save
         params[:vote] == 'up' ? @vote.comment.vote_up : @vote.comment.vote_down 
