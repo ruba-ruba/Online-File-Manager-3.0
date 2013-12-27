3701ab16725ea95b04638d7b9d98bb52b570ff99
ruba-ruba <gunit1991@ukr.net>
2013-12-27 12:15:40 +0200

    fix for votes

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,9 +1,10 @@
 class VotesController < ApplicationController
   def vote
     @vote = Vote.new(user_id: current_user.id, comment_id: params[:comment])
+        binding.pry
     respond_to do |format|
       if @vote.save
-        params[:vote] == 'up' ? vote.comment.vote_up : vote.comment.vote_down 
+        params[:vote] == 'up' ? @vote.comment.vote_up : @vote.comment.vote_down 
         format.js
       else
         format.js
