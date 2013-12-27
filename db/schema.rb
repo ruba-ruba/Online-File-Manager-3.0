5f7af1ea121c6210bb2e97768c03cd12d5479fcd
vladislav-gorbenko <gorbenko.vladislav@gmail.com>
2013-12-27 03:26:49 -0800

    refactoring schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-<<<<<<< Updated upstream
-ActiveRecord::Schema.define(:version => 20131227091358) do
-=======
 ActiveRecord::Schema.define(:version => 20131227110354) do
->>>>>>> Stashed changes
 
   create_table "comments", :force => true do |t|
     t.text     "content"
