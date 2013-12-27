0c5ef4fb7373596bcb3ed2cc5c9c9e22c2173d2e
ruba-ruba <gunit1991@ukr.net>
2013-12-27 18:51:42 +0200

    votes not finished

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,6 +1,6 @@
 class VotesController < ApplicationController
   def vote
-    @vote = Vote.new(user_id: current_user.id, comment_id: params[:comment])
+    @vote = Vote.find_by_user_id_and_comment_id_and_vote_type(current_user.id, params[:comment], params[:vote_type])
     respond_to do |format|
       if @vote.save
         params[:vote] == 'up' ? @vote.comment.vote_up : @vote.comment.vote_down
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131227142025) do
+ActiveRecord::Schema.define(:version => 20131227162711) do
 
   create_table "comments", :force => true do |t|
     t.text     "content"
@@ -83,6 +83,7 @@ ActiveRecord::Schema.define(:version => 20131227142025) do
     t.integer  "comment_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.boolean  "vote_type"
   end
 
 end
