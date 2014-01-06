37510229d2bd84beb4b4dce18d60056e9767240b
vladislav-gorbenko <gorbenko.vladislav@gmail.com>
2014-01-06 05:18:35 -0800

    try change migrate 2

--- db/migrate/20131227110354_change_column_to_comments.rb
+++ db/migrate/20131227110354_change_column_to_comments.rb
@@ -1,6 +1,11 @@
 class ChangeColumnToComments < ActiveRecord::Migration
   def change
     #change_column :comments, :user_id, :integer
-    execute "ALTER TABLE comments ALTER COLUMN user_id TYPE integer"
+    #execute "ALTER TABLE comments ALTER COLUMN user_id TYPE integer"
+    connection.execute(%q{
+      alter table comments
+      alter column user_id
+      type integer using cast(user_id as integer)
+    })
   end
 end
