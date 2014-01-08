770bb88e6c366abba7ec7d4022a1c929346289eb
vladislav-gorbenko <gorbenko.vladislav@gmail.com>
2014-01-08 05:15:19 -0800

    spec for Location

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,7 @@
 class Location < ActiveRecord::Base
   attr_accessible :address, :latitude, :longitude, :item_id
   belongs_to :item
+  
+  validates :latitude, :presence => true
+  validates :longitude, :presence => true
 end
