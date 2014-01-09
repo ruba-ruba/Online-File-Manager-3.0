312cfeee03d2dda8fd89209b63797ea105aeca7a
Maria Paschevska <maria.paschevska@gmail.com>
2014-01-09 16:24:14 +0200

    refactoring votes

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -6,4 +6,11 @@ class Vote < ActiveRecord::Base
 
   validates :user_id, uniqueness: { scope: :comment_id }
 
+  def self.revote(vote_type, comment_id, user_id)
+    comment = Comment.find(comment_id)
+    self.where(:user_id => user_id, :comment_id => comment_id).delete_all
+    vote = self.create(:user_id => user_id, :comment_id => comment_id, :vote_type => vote_type)
+    comment.update_counters
+    [comment, vote]
+  end
 end
