9c4db8db409a1dec6fff4e4470ab1cb3726bbf94
Maria Paschevska <mpasch@softserveinc.com>
2014-01-16 08:53:09 -0800

    search_files_controller_spec.rb added

--- spec/controllers/search_files_controller_spec.rb
+++ spec/controllers/search_files_controller_spec.rb
@@ -2,10 +2,22 @@ require 'spec_helper'
 
 describe SearchFilesController do
 
-  describe "GET 'index'" do
-    it "returns http success" do
-      get 'index'
-      response.should be_success
+  describe "Find existing files" do
+
+    let!(:item1){FactoryGirl.create(:item, file_file_name: 'ololo.txt',:folder => FactoryGirl.create(:folder))}
+    let!(:item2){FactoryGirl.create(:item, file_file_name: 'ttttt.txt',:folder => FactoryGirl.create(:folder))}
+    it "returns http success and founed items" do
+      get 'index', :q => {"file_file_name_cont" => "olo"}
+      expect(assigns(:q).result).to eq([item1])
+    end
+  end
+
+  describe "Find not existing files" do
+    let!(:item1){FactoryGirl.create(:item, file_file_name: 'hello.txt',:folder => FactoryGirl.create(:folder))}
+    let!(:item2){FactoryGirl.create(:item, file_file_name: 'world.txt',:folder => FactoryGirl.create(:folder))}
+    it "returns message" do      
+      get 'index', :q => {"file_file_name_cont" => "morning"}
+      expect(assigns(:q).result).to eq([])
     end
   end
 
