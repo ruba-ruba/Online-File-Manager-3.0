2e38371026ead9278b3345f40cccb9624dde93e2
vladislav-gorbenko <gorbenko.vladislav@gmail.com>
2014-01-17 20:17:46 +0200

    fix user_helper spec

--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -1,16 +1,17 @@
 module ControllerMacros
   def login_admin
     before(:each) do
-      @request.env["devise.mapping"] = Devise.mappings[:admin]
-      sign_in FactoryGirl.create(:admin) # Using factory girl as an example
+      @request.env["devise.mapping"] = Devise.mappings[:user]
+      @current_user = FactoryGirl.create(:admin)
+      sign_in :user, @current_user
     end
   end
 
   def login_user
     before(:each) do
       @request.env["devise.mapping"] = Devise.mappings[:user]
-      user = FactoryGirl.create(:user)
-      sign_in :user, user
+      @current_user = FactoryGirl.create(:user)
+      sign_in :user, @current_user
     end
   end
 end
