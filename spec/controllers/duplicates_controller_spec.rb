1b9d6751850412fdbc68df540aa72954504e7a9f
Maria Paschevska <mpasch@softserveinc.com>
2014-01-17 02:49:09 -0800

    add and update specs for duplicates_controller and search_files_controller

--- spec/controllers/duplicates_controller_spec.rb
+++ spec/controllers/duplicates_controller_spec.rb
@@ -2,11 +2,23 @@ require 'spec_helper'
 
 describe DuplicatesController do
   login_user
+  render_views
 
-  describe "GET index" do
-    it "assigns all Item.duplicates as @items" do
+  describe "GET index with duplicate files" do
+    let!(:item1){FactoryGirl.create(:item, file_file_name: '1.txt',:folder => FactoryGirl.create(:folder))}
+    let!(:item2){FactoryGirl.create(:item, file_file_name: '1.txt',:folder => FactoryGirl.create(:folder))}
+    it "assigns all Item.duplicates as @items and find duplicates" do
       get :index
       response.should be_success
+      response.body.should have_content("Item")
+    end
+  end
+
+  describe "GET index without any duplicate files" do
+    it "assigns all Item.duplicates as @items and cannot find any duplicates" do
+      get :index
+      response.should be_success
+      response.body.should have_content("There are no duplicate files")
     end
   end
 end
--- spec/controllers/search_files_controller_spec.rb
+++ spec/controllers/search_files_controller_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe SearchFilesController do
 
+  render_views
   describe "Find existing files" do
 
     let!(:item1){FactoryGirl.create(:item, file_file_name: 'ololo.txt',:folder => FactoryGirl.create(:folder))}
@@ -9,16 +10,17 @@ describe SearchFilesController do
     it "returns http success and founed items" do
       get 'index', :q => {"file_file_name_cont" => "olo"}
       expect(assigns(:q).result).to eq([item1])
+      response.body.should have_content("Name")
     end
   end
 
   describe "Find not existing files" do
     let!(:item1){FactoryGirl.create(:item, file_file_name: 'hello.txt',:folder => FactoryGirl.create(:folder))}
     let!(:item2){FactoryGirl.create(:item, file_file_name: 'world.txt',:folder => FactoryGirl.create(:folder))}
-    it "returns message" do      
+    it "returns message" do
       get 'index', :q => {"file_file_name_cont" => "morning"}
       expect(assigns(:q).result).to eq([])
+      response.body.should have_content("There is no files matching your request")
     end
   end
-
 end
