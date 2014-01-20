17e28d32fef8bcaf697e17574f4745c26d8ef073
vladislav-gorbenko <gorbenko.vladislav@gmail.com>
2014-01-20 11:30:53 -0800

    add rspec for users_api

--- spec/api/users_api_spec.rb
+++ spec/api/users_api_spec.rb
@@ -1,16 +1,37 @@
 require 'spec_helper'
 
 describe UsersApi::API do
+
   describe "GET /api/v1/:token" do
+    let(:user){FactoryGirl.create(:user)}
     it "returns an user info" do
-      user = FactoryGirl.create(:user)
       get "/api/v1/#{user.token}"
       response.status.should == 200
       JSON.parse(response.body).should == user.as_api.stringify_keys
     end
+  end
 
-    it 'returns message' do
-      
+  describe "def current user" do
+    let(:user){FactoryGirl.create(:user_wiht_token)}
+    it "returns current_user" do
+      get "/api/v1/#{user.token}"
+      expect(user.token).to eq('baM80EgOTSifODeo8TxgyA')
+    end
+  end  
+
+  describe "def current user" do 
+    let(:user){FactoryGirl.create(:user)}
+    it "returns errors" do
+      get "/api/v1/#{user.token}"
+      expect(user.token).not_to eq("baM80EgOTSifODeo8TxgyA")
+    end
+  end
+
+  describe "def current user" do 
+    let(:user){FactoryGirl.create(:user_wiht_expires)}
+    it "returns errors" do
+      get "/api/v1/#{user.token}"
+      response.status.should == 401
     end
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -14,5 +14,16 @@ FactoryGirl.define do
       user.update_attribute(:role, 'admin')
     end
   end
-  
+
+  factory :user_wiht_token, :parent => :user do
+    after(:create) do |user|
+      user.update_attribute(:token, 'baM80EgOTSifODeo8TxgyA')
+    end
+  end
+
+  factory :user_wiht_expires, :parent => :user do
+    after(:create) do |user|
+      user.update_attribute(:expires_at, DateTime.now-400)
+    end
+  end
 end
