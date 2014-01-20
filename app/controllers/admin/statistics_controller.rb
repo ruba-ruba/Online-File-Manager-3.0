4aa9c365bddd1ba5d548a17ac28ef5185f55dddb
Maria Paschevska <mpasch@softserveinc.com>
2014-01-20 01:46:20 -0800

    highchart shows real files data

--- app/controllers/admin/statistics_controller.rb
+++ app/controllers/admin/statistics_controller.rb
@@ -1,6 +1,24 @@
 class Admin::StatisticsController < ApplicationController
 
   def index
-    
+    images = Item.where("file_file_name like (?)", "%.jpg")
+    @images_sum = images.pluck(:file_file_size).inject{|sum, x| sum + x }
+    @images_count = images.count
+
+    video = Item.where("file_file_name like (?)", "%.mp4")
+    @video_sum = video.pluck(:file_file_size).inject{|sum, x| sum + x }
+    @video_count = video.count
+
+    music = Item.where("file_file_name like (?)", "%.mp3")
+    @music_sum = music.pluck(:file_file_size).inject{|sum, x| sum + x}
+    @music_count = music.count
+
+    texts = Item.where("file_file_name like (?)", "%.txt")
+    @texts_sum = texts.pluck(:file_file_size).inject{|sum, x| sum + x}
+    @texts_count = texts.count
+
+    maps = Item.where("file_file_name like (?)", "%.csv")
+    @maps_sum = maps.pluck(:file_file_size).inject{|sum, x| sum + x}
+    @maps_count = maps.count
   end
 end
--- app/helpers/search_files_helper.rb
+++
@@ -1,2 +0,0 @@
-module SearchFilesHelper
-end
