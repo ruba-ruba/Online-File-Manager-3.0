9abe5a9aee97480afa29bb28174883ceb2fdb512
vladislav-gorbenko <gorbenko.vladislav@gmail.com>
2014-01-23 09:30:31 -0800

    add specs for folder

--- spec/factories/folders.rb
+++ spec/factories/folders.rb
@@ -3,6 +3,10 @@ FactoryGirl.define do
     title {generate(:folder)}
   end
 
+  factory :folder_with_title, :class => 'Folder' do
+    title "video"
+  end
+
   factory :folder_with_user, :class => 'Folder' do
     title {generate(:folder)}
     association :user
--- spec/models/folder_spec.rb
+++ spec/models/folder_spec.rb
@@ -1,10 +1,32 @@
 require 'spec_helper'
 
 describe Folder do
-  let(:folder){FactoryGirl.create(:folder)}
+  
   describe "#size" do
+    let(:folder){FactoryGirl.create(:folder)}
     it "should return size" do
-      expect(folder.size).to eq(0)  
-    end 
+      expect(folder.size).to eq(0)
+    end
+  end
+  describe "#zip_archive" do
+    let(:folder){FactoryGirl.create(:folder_with_title)}
+    let(:user){FactoryGirl.create(:user)}
+    it "should return url" do
+      expect(folder.zip_archive(user.id)).should_not be_nil
+    end
+    it "should return true url" do
+      fake_file = stub(:url => 'resulting_url')
+      fake_item = stub(:file =>fake_file)
+      fake_zip = stub(:close => true)
+
+      folder.expects(:add_to_zip).returns(true)
+      Zip::File.expects(:open).returns(fake_zip)
+      File.expects(:new).returns('file_with_data')
+      File.expects(:delete).returns(true)
+      Item.expects(:create).with(folder_id: nil, user_id: user.id, file: 'file_with_data').returns  (fake_item)
+
+      expect(folder.zip_archive(user.id)).to eq('resulting_url')
+    end
+
   end
 end
