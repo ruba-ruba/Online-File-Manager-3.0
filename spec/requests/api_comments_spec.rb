e0bb720ea596e9935b686d53ea35790632618ad5
vladislav-gorbenko <gorbenko.vladislav@gmail.com>
2014-02-04 03:13:26 -0800

    refactoring rspec for api_comments

--- spec/requests/api_comments_spec.rb
+++ spec/requests/api_comments_spec.rb
@@ -2,12 +2,12 @@ require 'spec_helper'
 
 describe FileManager::API do
 
-  login_user
+  let(:user){FactoryGirl.create :user}
 
   describe "GET /api/v3/folders/id/comments" do
     it "returns an empty array of folders" do
       folder = FactoryGirl.create :folder
-      get "/api/v3/folders/#{folder.id}/comments"
+      get "/api/v3/folders/#{folder.id}/comments?token=#{user.token}"
       response.status.should == 200
       JSON.parse(response.body).should == []
     end
@@ -16,7 +16,7 @@ describe FileManager::API do
   describe "GET /api/v3/items/id/comments" do
     it "returns an empty array of items" do
       item = FactoryGirl.create :item
-      get "/api/v3/items/#{item.id}/comments"
+      get "/api/v3/items/#{item.id}/comments?token=#{user.token}"
       response.status.should == 200
       JSON.parse(response.body).should == []
     end
@@ -24,8 +24,8 @@ describe FileManager::API do
 
   describe "GET /api/v3/items/id/comments" do
     it "returns an id for items" do
-      comment = FactoryGirl.create :item_comment, :user_id => @current_user.id   
-      get "/api/v3/items/#{comment.commentable_id}/comments"
+      comment = FactoryGirl.create :item_comment, :user_id => user.id
+      get "/api/v3/items/#{comment.commentable_id}/comments?token=#{user.token}"
       response_hash = JSON.parse(response.body)[0]
       expect(response_hash["id"]).to eq(comment.id)
     end
@@ -33,8 +33,8 @@ describe FileManager::API do
 
   describe "GET /api/v3/items/id/comments" do
     it "returns an user_id for items" do
-      comment = FactoryGirl.create :item_comment, :user_id => @current_user.id   
-      get "/api/v3/items/#{comment.commentable_id}/comments"
+      comment = FactoryGirl.create :item_comment, :user_id => user.id
+      get "/api/v3/items/#{comment.commentable_id}/comments?token=#{user.token}"
       response_hash = JSON.parse(response.body)[0]
       expect(response_hash["user_id"]).to eq(comment.user_id)
     end
@@ -42,8 +42,8 @@ describe FileManager::API do
 
   describe "GET /api/v3/items/id/comments" do
     it "returns a content for items" do
-      comment = FactoryGirl.create :item_comment, :user_id => @current_user.id, :content => "Hello"   
-      get "/api/v3/items/#{comment.commentable_id}/comments"
+      comment = FactoryGirl.create :item_comment, :user_id => user.id, :content => "Hello"
+      get "/api/v3/items/#{comment.commentable_id}/comments?token=#{user.token}"
       response_hash = JSON.parse(response.body)[0]
       expect(response_hash["content"]).to eq("Hello")
     end
@@ -51,8 +51,8 @@ describe FileManager::API do
 
   describe "GET /api/v3/items/id/comments" do
     it "returns a votes_up for items" do
-      comment = FactoryGirl.create :item_comment, :user_id => @current_user.id, :votes_up => 1   
-      get "/api/v3/items/#{comment.commentable_id}/comments"
+      comment = FactoryGirl.create :item_comment, :user_id => user.id, :votes_up => 1
+      get "/api/v3/items/#{comment.commentable_id}/comments?token=#{user.token}"
       response_hash = JSON.parse(response.body)[0]
       expect(response_hash["votes_up"]).to eq(comment.votes_up)
     end
@@ -60,8 +60,8 @@ describe FileManager::API do
 
   describe "GET /api/v3/items/id/comments" do
     it "returns a votes_down for items" do
-      comment = FactoryGirl.create :item_comment, :user_id => @current_user.id, :votes_down => 1   
-      get "/api/v3/items/#{comment.commentable_id}/comments"
+      comment = FactoryGirl.create :item_comment, :user_id => user.id, :votes_down => 1
+      get "/api/v3/items/#{comment.commentable_id}/comments?token=#{user.token}"
       response_hash = JSON.parse(response.body)[0]
       expect(response_hash["votes_down"]).to eq(comment.votes_down)
     end
@@ -69,8 +69,8 @@ describe FileManager::API do
 
   describe "GET /api/v3/items/id/comments" do
     it "returns an author for items" do
-      comment = FactoryGirl.create :item_comment, :user_id => @current_user.id  
-      get "/api/v3/items/#{comment.commentable_id}/comments"
+      comment = FactoryGirl.create :item_comment, :user_id => user.id
+      get "/api/v3/items/#{comment.commentable_id}/comments?token=#{user.token}"
       response_hash = JSON.parse(response.body)[0]
       expect(response_hash["author"]).to eq("anonymous")
     end
@@ -78,8 +78,8 @@ describe FileManager::API do
 
   describe "GET /api/v3/items/id/comments" do
     it "returns a no_parent_identifier for items" do
-      comment = FactoryGirl.create :item_comment, :user_id => @current_user.id  
-      get "/api/v3/items/#{comment.commentable_id}/comments"
+      comment = FactoryGirl.create :item_comment, :user_id => user.id
+      get "/api/v3/items/#{comment.commentable_id}/comments?token=#{user.token}"
       response_hash = JSON.parse(response.body)[0]
       expect(response_hash["parent_identifier"]).to eq(".comments>ul")
     end
@@ -87,9 +87,9 @@ describe FileManager::API do
 
   describe "GET /api/v3/items/id/comments" do
     it "returns a parent_identifier for items" do
-      comment = FactoryGirl.create :item_comment, :user_id => @current_user.id 
-      comment = FactoryGirl.create :item_comment, :user_id => @current_user.id, :parent_id => comment.id  
-      get "/api/v3/items/#{comment.commentable_id}/comments"
+      comment = FactoryGirl.create :item_comment, :user_id => user.id
+      comment = FactoryGirl.create :item_comment, :user_id => user.id, :parent_id => comment.id  
+      get "/api/v3/items/#{comment.commentable_id}/comments?token=#{user.token}"
       response_hash = JSON.parse(response.body)[0]
       expect(response_hash["parent_identifier"]).to eq(".comment-#{comment.parent.id}")
     end
@@ -97,8 +97,8 @@ describe FileManager::API do
 
   describe "GET /api/v3/items/id/comments" do
     it "returns a created_date_time for items" do
-      comment = FactoryGirl.create :item_comment, :user_id => @current_user.id 
-      get "/api/v3/items/#{comment.commentable_id}/comments"
+      comment = FactoryGirl.create :item_comment, :user_id => user.id
+      get "/api/v3/items/#{comment.commentable_id}/comments?token=#{user.token}"
       response_hash = JSON.parse(response.body)[0]
       expect(response_hash["created_date_time"]).to eq(comment.created_at.strftime("%b %e, %l:%M %p"))
     end
